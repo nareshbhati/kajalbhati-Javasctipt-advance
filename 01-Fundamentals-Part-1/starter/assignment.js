2882d84c2e031071ccc9cbd7fcc667acd8db91ed
Naresh Bhati <nbhati@finicity.corp>
2022-12-11 11:16:05 +0530

    kajak-first-commit

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,22 @@
+// let population = "7000";
+// let continent = "Asia";
+// let country = "India";
+// console.log(population);
+// console.log(continent);
+// console.log(country);
+// console.log(population, continent, country);
+
+const markH = 1.95;
+//test1 //test2//challenge1
+const markW = 92;
+const markBMI = markW / markH ** 2;
+const johnH = 1.69;
+const johnW = 78;
+const johnBMI = johnW / johnH ** 2;
+// const markHigherBMI = markBMI > johnBMI;
+// console.log("markHigherBMI",markHigherBMI);
+if ( markBMI > johnBMI) {
+  console.log(`Mark's BMI ${markBMI} is higher than John's BMI ${johnBMI}`);
+} else {
+  console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+} //challenge2
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,110 @@
+// let a = "morning";
+// if (a === "morning") alert("night");
+// console.log(40 + 50 - 10);
+
+//chapter10 value$variable
+// let gender="male";
+// console.log(gender);
+//chapter12
+// let firstPerson="kajal";
+// console.log(firstPerson);
+// let isMale=false;
+// console.log(isMale);
+// console.log(typeof false);
+// console.log(typeof isMale);
+
+//chapter13 let,const,var
+// let num=21;
+// console.log(typeof num);
+// let age;
+// console.log(typeof age);
+
+// const birthYear=1996;
+// console.log(birthYear);
+
+//var
+// if (true) {
+//   let  a = 10;
+//   console.log("inside function", a);
+// }
+
+// console.log("outside funvtion", a);
+
+//chapter14$15 operators
+// const firstName="kajal";
+// const secondName="naresh";
+// console.log(firstName+" "+secondName);
+
+// let x=21;
+// const y=4;
+// console.log(x*y,x-y,x**y);
+
+// console.log(x+=y);// x=x+y
+// console.log(x--);
+
+//chapter17 string$ template lateral backticks
+// const first="yashvi";
+// const second="playgroup";
+// const age=2;
+// const yashu=first+" is "+age+" years old in "+second;
+// console.log(yashu);
+
+// const yashuNew=`${first} is ${age} years old in ${second}`;
+// console.log(yashuNew);
+
+// console.log("yashvi\n\is\n\good \n\girl");
+// console.log(`yashvi
+// is
+// good
+// girl`);
+
+//chapter18 if/else
+// const age = 3;
+// if (age >= 3) {
+//   console.log(`admission will be taken`);
+// } else {
+//   console.log(`admission not taken`);
+// }
+
+//chapter20
+//conversion
+// const x=1996;
+// console.log(x+"21");
+// console.log(x+Number('21'));
+// console.log(String(21)+"jonas");
+// console.log(Number("kajal"));
+// //coercion
+// console.log('21'+19);//2119
+// console.log("kajal"+"naresh");
+// console.log("45"-"15"-10);//20//-always consider string as number
+// console.log("I am "+26+" years old");
+// console.log(19+1+"hie");//20hie
+// console.log("hie"+19+1);//hie191
+// console.log(null+21);
+// console.log(typeof null);
+// console.log(undefined+1);
+
+// let k="21"+4;
+// k=k+1;
+// console.log(k);
+
+//chapter21
+// 5 falsy value- 0,"",undefined,null,NaN
+console.log(Boolean(null));
+console.log(Boolean(0));
+console.log(Boolean(""));
+
+if(0){
+    console.log("fail");
+}
+else{
+    console.log("pass");
+}
+
+let x;
+if(x){
+    console.log("variable is defined");
+}
+else{
+    console.log("variable is undefined");
+}
