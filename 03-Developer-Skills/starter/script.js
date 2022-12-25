c929ba2af40c5ee3dc3d9566314de54c119fff65
Naresh Bhati <nbhati@finicity.corp>
2022-12-26 00:18:51 +0530

    stackoverflow

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const num = 56;
+console.log(num);
+
+console.log("hiee!");
+
+// const temp = [3, 2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+const tempOne = [3, 2, -6, -1, "error"];
+const tempTwo = [9, 13, 17, 15, 14, 9, 5];
+const temp = tempOne.concat(tempTwo);
+console.log(temp);
+const calcTemp = function (temp) {
+  let max = temp[0];
+  let min = temp[0];
+  for (let i = 1; i < temp.length; i++) {
+    if (typeof temp[i] !== "number") continue;
+    if (max < temp[i]) max = temp[i];
+    if (min > temp[i]) min = temp[i];
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTemp(temp);
+console.log(amplitude);
