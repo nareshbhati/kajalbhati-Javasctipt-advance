73b002ec3dca8dd147abb3f9cf82be138407ade0
Naresh Bhati <nbhati@finicity.corp>
2023-01-08 23:58:34 +0530

    project3

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+const score1E = document.querySelector('#score--0');
+const score2E = document.getElementById('score--1');
+const newGame = document.querySelector('.btn--new');
+const rollDice = document.querySelector('.btn--roll');
+const hold = document.querySelector('.btn--hold');
+const current1E = document.getElementById('current--0');
+const current2E = document.getElementById('current--1');
+const player1E = document.querySelector('.player--0');
+const player2E = document.querySelector('.player--1');
+
+// all set to zero
+score1E.textContent = 0;
+score2E.textContent = 0;
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+const diceE = document.querySelector('.dice');
+diceE.classList.add('hidden');
+rollDice.addEventListener('click', function () {
+  //rolling a dice
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // displaying dice
+  diceE.classList.remove('hidden');
+  diceE.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current1E.textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player1E.classList.toggle('player--active');
+    player2E.classList.toggle('player--active');
+  }
+});
