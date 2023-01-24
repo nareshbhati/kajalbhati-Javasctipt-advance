5295e74efd12d5464dc2a6368de5d3ec5dc201b3
Naresh Bhati <nbhati@finicity.corp>
2023-01-25 00:00:12 +0530

    behind the scene

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+// SCOPE chain
+// console.log(firstName);
+// const firstName = 'kajal';
+// const calcAge = function (birthYear) {
+//   const age = 2022 - birthYear;
+//   //   return age;
+
+//   if (age > 18) {
+//     var newAge = 27;
+//     // console.log(newAge);
+//   }
+//   console.log(newAge);
+//   return age;
+// };
+// // console.log(currentAge);
+// const currentAge = calcAge(1996);
+// console.log(currentAge);
+
+// hoisting
+// console.log(city);
+// // console.log(myName);
+// // console.log(age);
+
+// const myName = 'kajal';
+// let age = 26;
+// var city = 'mumbai';
+
+// // console.log(kajalAge(1996));
+// // console.log(kajalAge2(26));
+// // console.log(kajalAge3(2023));
+
+// function kajalAge(birthyear) {
+//   const age = 2022 - birthyear;
+//   return age;
+// }
+
+// const kajalAge2 = function (year) {
+//   return year;
+// };
+
+// var kajalAge3 = currentYear => 2022 - currentYear;
+
+// var
+if (!currentAge) kajal();
+
+var currentAge = 26;
+function kajal() {
+  console.log('kajal is 26 years old');
+}
+
+// variable with var create a property on GLOBAL WINDOW OBJECT
+
+var x = 28;
+console.log(window.x === x);
+
+const y = 28;
+console.log(window.y === y);
