b03193bea9ce8cb7f6b866de3c1065c1fbb08d75
Naresh Bhati <nbhati@finicity.corp>
2023-01-26 00:25:29 +0530

    this keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -42,17 +42,58 @@
 // var kajalAge3 = currentYear => 2022 - currentYear;
 
 // var
-if (!currentAge) kajal();
+// if (!currentAge) kajal();
 
-var currentAge = 26;
-function kajal() {
-  console.log('kajal is 26 years old');
-}
+// var currentAge = 26;
+// function kajal() {
+//   console.log('kajal is 26 years old');
+// }
 
-// variable with var create a property on GLOBAL WINDOW OBJECT
+// // variable with var create a property on GLOBAL WINDOW OBJECT
 
-var x = 28;
-console.log(window.x === x);
+// var x = 28;
+// console.log(window.x === x);
 
-const y = 28;
-console.log(window.y === y);
+// const y = 28;
+// console.log(window.y === y);
+
+// this keyword
+console.log(this);
+
+const agefunc = function (birthYear) {
+  const age = 2022 - birthYear;
+  console.log(this);
+  // this keyword shows undefined in strict mode and global window in regular mode
+  return age;
+};
+agefunc();
+
+const kajalAge = birthYear => {
+  console.log(2022 - birthYear);
+  console.log(this);
+};
+kajalAge(1996);
+
+const kajal = {
+  firstName: 'kajal',
+  year: 1996,
+  calcAge: function () {
+    const age = 2022 - this.year;
+    console.log(this);
+    console.log(age);
+    return age;
+  },
+};
+kajal.calcAge();
+
+const naresh = {
+  year: 1992,
+};
+// method borrowing
+naresh.calcAge = kajal.calcAge;
+// this always points to object for which function is calling
+naresh.calcAge();
+
+const newFunc = kajal.calcAge;
+// here 'newFunc' behave like just a regular function so in strict mode this keyword points to undefine
+newFunc();
