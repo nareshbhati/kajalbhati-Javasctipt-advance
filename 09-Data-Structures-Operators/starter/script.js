cee3bd54e536551a82a44bc4ee28db0a9a7348d1
Naresh Bhati <nbhati@finicity.corp>
2023-02-02 15:41:56 +0530

    destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +30,68 @@ const restaurant = {
     },
   },
 };
+
+// destructuring of an array
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+// can skip an element of array by using a hole or space
+let [first, , second] = restaurant.categories;
+console.log(first, second);
+
+// switching of variable value
+// const temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+[first, second] = [second, first];
+console.log(first, second);
+
+console.log(restaurant.order(2, 0));
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// nested array
+const nested = [2, 3, [4, 5]];
+const [i, j] = nested;
+console.log(i, j);
+const [p, , q] = nested;
+console.log(p, q);
+const [r, , [s, t]] = nested;
+console.log(r, s, t);
+
+// default values ,here value of 'o' has been set 1 as default value
+const [m = 1, n = 1, o = 1] = [7, 8];
+console.log(m, n, o);
+
+// destructuring of an object ,order of properties are not mandantory
+const { name, openingHours, categories } = restaurant;
+console.log(name, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(hours, tags);
+
+// default falues to property
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// switchins or mutating values
+let e = 111;
+let f = 222;
+const obj = { e: 21, f: 23, g: 25 };
+({ e, f } = obj);
+console.log(e, f);
