9a7e39405a91f25fae4382897bb67f9c5d07b725
Naresh Bhati <nbhati@finicity.corp>
2023-04-04 00:27:43 +0530

    call,apply,bind

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -110,9 +110,8 @@ const airIndia = {
       flight: `${this.code}${flightNum}`,
       name,
     });
+    console.log(this.booking);
   },
-  console.log(booking);
-
 };
 airIndia.book(458, 'kajal');
 
@@ -134,3 +133,57 @@ const flightData = [785, 'naresh'];
 flightBooking.apply(airIndia, flightData);
 
 flightBooking.call(airIndia, ...flightData);
+
+// bind method(in this function is not immediately called,it create a new function which will be called as we want)
+const bookSJ = flightBooking.bind(spiceJet);
+const bookAI = flightBooking.bind(airIndia);
+
+bookSJ(78, 'pooja');
+bookAI(787, 'bhumika');
+
+// here we can set arguement predefine before function call
+const book2SJ = flightBooking.bind(spiceJet, 456);
+book2SJ('kajal Naresh Bhati');
+book2SJ('Naresh Bhati');
+
+// with Event Listener(here,this keyword always points to the element for which handler function is called)
+airIndia.planes = 129;
+airIndia.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+airIndia.buyPlane();
+// we have to set 'this' keyword manually by using bind because call immediately call the function
+document
+  .querySelector('.buy')
+  .addEventListener('click', airIndia.buyPlane.bind(airIndia));
+
+// partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(550));
+console.log(addVAT(105));
+
+// function returning function
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const add = addTaxRate(0.1);
+console.log(add(200));
+
+// IIFE(FUNCTION WHICH ARE called once and never used again )
+
+(function () {
+  const i = 'kajal naresh bhati';
+  console.log(i.slice(0, 5));
+  console.log(i.padStart(20, '*'));
+  console.log(i.split(' '));
+})();
+
+// bt nowadays there is no need to use IIFE because code block also donot allow to access data or variable outside the codeblock
